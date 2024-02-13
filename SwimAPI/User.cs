109beec6f76a1fbf4cde70c9e91dfd238b7b4f75
Pedro <xxferreira24@gmail.com>
2024-02-13 16:53:05 +0000

    Adicionar arquivos de projeto.

---
+++ SwimAPI/User.cs
@@ -0,0 +1,12 @@
+﻿namespace SwimAPI
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+
+    }
+}
